b61c63875984276e34b3b25c12dc5a6e9bf49e7c
Neel Shah <neelshah400@gmail.com>
2018-10-28 11:39:27 -0400

    Fix URLs

--- js/index.js
+++ js/index.js
@@ -20,7 +20,22 @@ function contentLoader(){
     document.getElementById(file).classList.add("active");
 };
 
-function resizeIframe(obj) {
-    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+function doIframe() {
+    var $iframes = $("iframe.autoHeight"); 
+    $iframes.each(function() {
+        var iframe = this;
+        $(iframe).load(function() {
+            setHeight(iframe);
+        });
+    });
 }
+
+function setHeight(e) {
+  e.height = e.contentWindow.document.body.scrollHeight + 35;
+}
+
+$(window).on('load', function() {
+    doIframe();
+});
+
 
