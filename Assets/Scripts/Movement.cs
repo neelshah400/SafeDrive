15cf5c20ca6f3cc73b61920d992435508b4d587a
Neel Shah <neelshah400@gmail.com>
2018-11-06 13:43:41 -0500

    Added car/controls

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public GameObject butL;
+    public GameObject butR;
+    private float moveDir;
+    private float factor;
+    public float moveSpeed = 100;
+    public bool left = false;
+    public bool right = false;
+
+    // Use this for initialization
+    void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        factor = 0;
+        if (Input.GetAxis("Horizontal") == -1)
+        {
+            factor = -1;
+        }
+        if (Input.GetAxis("Horizontal") == 1)
+        {
+            factor = 1;
+        }
+        if (left == true)
+        {
+            factor = -1;
+        }
+        if (right == true)
+        {
+            factor = 1;
+        }
+        moveDir = factor * moveSpeed;
+        rb.velocity = new Vector2(moveDir, rb.velocity.y);
+
+    }
+
+    public void setLeftTrue() { left = true; }
+    public void setLeftFalse() { left = false; }
+    public void setRightTrue() { right = true; }
+    public void setRightFalse() { right = false; }
+
+}
